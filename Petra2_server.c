30c050af77c8b5ca7f6699c4651ffb096b59c838
Yann-McRissos <citrouillon@skynet.be>
2020-10-15 10:48:05 -0700

    ça marche pas

--- Petra2_server.c
+++ Petra2_server.c
@@ -34,7 +34,7 @@ struct CAPTEURS
 	unsigned PP : 1;
 	unsigned DE : 1;
 	unsigned CS : 1; //anciennement CS
-	/*       unsigned H1 : 1;
+					 /*       unsigned H1 : 1;
 		 unsigned H2 : 1;
 		 unsigned H3 : 1;
 		 unsigned H4 : 1;
@@ -56,33 +56,51 @@ union
 	unsigned char byte;
 } u_capt;
 
+#define CHARIOT u_act.act.CP
+#define CONVOYEUR1 u_act.act.C1
+#define CONVOYEUR2 u_act.act.C2
+#define VENTOUSE u_act.act.PV
+#define PLONGEUR u_act.act.PA
+#define ARBRE u_act.act.AA
+#define GRAPIN u_act.act.GA
+
+#define L1 u_capt.capt.L1
+#define L2 u_capt.capt.L2
+#define EPAISSEUR u_capt.capt.T
+#define SLOT u_capt.capt.S
+#define CHARIOTSTABLE u_capt.capt.CS
+#define BRAS u_capt.capt.AP
+#define CAPTPLONGEUR u_capt.capt.PP
+#define BAC u_capt.capt.DE
+
 #define PORT 40000
 #define MAXSTRING 1
 #define nbConnexion 1
 
 int initSocket(const char *IP, int port, struct sockaddr_in *adresseSocket);
-/* Creates and returns a server socket for the machine identified by hostname on the Network Port port */
 int createServerSocket(const char *IP, int port, struct sockaddr_in *adresseSocket);
 void listenSocket(int hSocket);
 int acceptSocket(int hSocket, struct sockaddr *adresseSocket);
 
+void handlerSIGINT(int sig);
 void *threadCapteurs(void *);
 void ouverturePetra();
 void sendMsg(char msg);
 int recvMsg();
 int menuPetra(int actuateur);
 
-int hSocketService;
+int hSocketService, hSocketEcoute;
 int fd_petra_in, fd_petra_out;
+struct sigaction act;
 
 int main()
 {
-	int hSocketEcoute, quitter = 0;
+	int quitter = 0;
 	struct sockaddr_in adresseSocket;
 	struct in_addr adresseIP;
 	struct hostent *infosHost;
 	pthread_t tid;
-	char *IP = malloc(17); char msg;
+	char *IP = malloc(17);
 
 	// Informations machine
 	if ((infosHost = gethostbyname("ubuntu")) == 0)
@@ -96,27 +114,35 @@ int main()
 	// Création socket
 	hSocketEcoute = createServerSocket(IP, PORT, (struct sockaddr_in *)&adresseSocket);
 	printf("Creation socket OK\n");
+
+	// Armement sur le signal SIGINT
+	act.sa_handler = handlerSIGINT;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGINT, &act, 0);
+	// Ouverture de la communication avec le PETRA
 	//ouverturePetra();
 
 	while (1)
 	{
 		listenSocket(hSocketEcoute);
 		printf("listen OK\n");
-		if ((hSocketService = acceptSocket(hSocketEcoute, (struct sockaddr *)&adresseSocket))== -1)
+		if ((hSocketService = acceptSocket(hSocketEcoute, (struct sockaddr *)&adresseSocket)) == -1)
 		{
 			printf("erreur accept : %s\n", strerror(errno));
 			exit(1);
 		}
 		printf("accept OK\n");
-		if(pthread_create(&tid, NULL, threadCapteurs, &hSocketService) == -1)
+		if (pthread_create(&tid, NULL, threadCapteurs, &hSocketService) == -1)
 		{
 			printf("Erreur de creation de thread : %s\n", strerror(errno));
 			exit(1);
 		}
 
-		while (quitter != 8)
+		while (quitter)
 		{
-			msg = recvMsg();
+			if(recvMsg() == -1)
+				printf("Erreur de reception: %d\n", errno);
 			//quitter = menuPetra(msg);
 		}
 		pthread_kill(tid, SIGUSR1);
@@ -129,96 +155,87 @@ int main()
 
 int menuPetra(int actuateur)
 {
-	read(fd_petra_out, &u_act.byte, 1);
-
 	printf("Actuateurs: %d\n", actuateur);
 	fflush(stdin);
-
-	if (actuateur == 1)
+	// pas nécessaire
+	read(fd_petra_out, &u_act.byte, 1);
+	switch (actuateur)
 	{
-		printf("IF 1");
+	case 0: // QUITTER
+		return 0;
+		break;
+	case 1: // Conv1
 		if (u_act.act.C1 == 0)
 			u_act.act.C1 = 1;
 		else
 			u_act.act.C1 = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 2)
-	{
-		printf("IF 2");
+		break;
+	case 2: // Conv2
 		if (u_act.act.C2 == 0)
 			u_act.act.C2 = 1;
 		else
 			u_act.act.C2 = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 3)
-	{
-		printf("IF 3");
+		break;
+	case 3: // Ventouse
 		if (u_act.act.PV == 0)
 			u_act.act.PV = 1;
 		else
 			u_act.act.PV = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 4)
-	{
-		printf("IF 4");
+		break;
+	case 4: // Plongueur
 		if (u_act.act.PA == 0)
 			u_act.act.PA = 1;
 		else
 			u_act.act.PA = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 5)
-	{
-		printf("IF 5");
+		break;
+	case 5: // Arbre
 		if (u_act.act.AA == 0)
 			u_act.act.AA = 1;
 		else
 			u_act.act.AA = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 6)
-	{
-		printf("IF 6");
+		break;
+	case 6: // Grappin
 		if (u_act.act.GA == 0)
 			u_act.act.GA = 1;
 		else
 			u_act.act.GA = 0;
 		write(fd_petra_out, &u_act.byte, 1);
-	}
-	else if (actuateur == 7)
-	{
-		printf("--------Position du bras--------");
-		printf("\n1. Réservoir");
-		printf("\n2. Tapis 1");
-		printf("\n3. Entre les 2 tapis");
-		printf("\n4. Tapis 2");
-		printf("\n--------------------------------\n");
-
-		fflush(stdin);
-		actuateur = recvMsg();
-		switch (actuateur)
-		{
-		case 1:
-			if (u_act.act.CP != 0)
-				u_act.act.CP = 0; // 10
-			break;
-		case 2:
-			if (u_act.act.CP != 1)
-				u_act.act.CP = 1; // 01
-			break;
-		case 3:
-			if (u_act.act.CP != 2)
-				u_act.act.CP = 2; // 10
-			break;
-		case 4:
-			if (u_act.act.CP != 3)
-				u_act.act.CP = 3; // 11
-		}
+		break;
+	case 7: // Réservoir
+		if ((u_act.act.CP & 0x11) == 0)
+			u_act.act.CP |= 0x00;
+		else
+			u_act.act.CP &= 0x11;
 		write(fd_petra_out, &u_act.byte, 1);
+		break;
+	case 8: // Tapis 1
+		if ((u_act.act.CP & 0x10) == 0)
+			u_act.act.CP |= 0x01;
+		else
+			u_act.act.CP &= 0x10;
+		write(fd_petra_out, &u_act.byte, 1);
+		break;
+	case 9: // Bac KO
+		if ((u_act.act.CP & 0x01) == 0)
+			u_act.act.CP |= 0x10;
+		else
+			u_act.act.CP &= 0x01;
+		write(fd_petra_out, &u_act.byte, 1);
+		break;
+	case 10: // Tapis 2
+		if ((u_act.act.CP & 0x00) == 0)
+			u_act.act.CP |= 0x11;
+		else
+			u_act.act.CP &= 0x00;
+		write(fd_petra_out, &u_act.byte, 1);
+		break;
 	}
+
 	return actuateur;
 }
 
@@ -230,15 +247,15 @@ void *threadCapteurs(void *s)
 
 	while (1)
 	{
-		read(fd_petra_in, &u_capt.byte, 1);
+		//read(fd_petra_in, &u_capt.byte, 1);
 
-		sendMsg(u_capt.capt.DE);
-		sendMsg(u_capt.capt.CS);
-		sendMsg(u_capt.capt.PP);
-		sendMsg(u_capt.capt.S);
-		sendMsg(u_capt.capt.L1);
-		sendMsg(u_capt.capt.L2);
-		sendMsg(u_capt.capt.AP);
+		sendMsg(BAC);
+		sendMsg(CHARIOTSTABLE);
+		sendMsg(PLONGEUR);
+		sendMsg(SLOT);
+		sendMsg(L1);
+		sendMsg(L2);
+		sendMsg(BRAS);
 	}
 }
 
@@ -324,12 +341,14 @@ int createServerSocket(const char *IP, int port, struct sockaddr_in *adresseSock
 	/* Le système prend connaissance de l'adresse et du port de la socket */
 	if (bind(hSocket, (struct sockaddr *)adresseSocket, sizeof(struct sockaddr_in)) == -1)
 	{
-		switch(errno)
+		switch (errno)
 		{
-			case EADDRINUSE: printf("Error: Socket is in use\n");
-				break;
-			case EINVAL: printf("Error: Socket already bound to an address\n");
-				break;
+		case EADDRINUSE:
+			printf("Error: Socket is in use\n");
+			break;
+		case EINVAL:
+			printf("Error: Socket already bound to an address\n");
+			break;
 		}
 		exit(1);
 	}
@@ -363,4 +382,14 @@ int acceptSocket(int hSocket, struct sockaddr *adresseSocket)
 	//printf("Accept socket OK\n");
 
 	return hSocketService;
+}
+
+void handlerSIGINT(int sig)
+{
+	printf("\nHANDLER: reçu %d\n", sig);
+	close(hSocketEcoute);
+	close(hSocketService);
+	printf("Sockets fermees\n");
+	puts("Fin du serveur...\n");
+	exit(1);
 }
